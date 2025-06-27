ed4b1fc236c9c7d2b5fffb4a24a2d1869cc86ff2
Marc-Aurele Coste <c.marc.aurele@outlook.com>
2025-06-27 11:49:03 +0200

    Update project

--- FormationWeb.WebAPIPro/Controllers/UserController.cs
+++ FormationWeb.WebAPIPro/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 using FormationWeb.Application.Contracts;
 using FormationWeb.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FormationWeb.WebAPIPro.Controllers
 {
@@ -14,7 +15,50 @@ namespace FormationWeb.WebAPIPro.Controllers
         {
             _service = service;
         }
-        
-        
+
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateUser([FromBody] User user)
+        {
+            if (user is null)
+                return BadRequest(new { message = "User cannot be null" });
+
+            try
+            {
+                return Ok(await _service.CreateAsync(user));
+            }
+            catch (DbUpdateException e)
+            {
+                return StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    new { message = $"Database Error : {e.Message}" });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    new { message = $"Unexpected Error : {e.Message}" });
+            }
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<User>> DeleteUser(Guid id)
+        {
+            try
+            {
+                return Ok(await _service.DeleteAsync(id));
+            }
+            catch (DbUpdateException e)
+            {
+                return StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    new { message = $"Database Error : {e.Message}" });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    new { message = $"Unexpected Error : {e.Message}" });
+            }
+        }
     }
 }
